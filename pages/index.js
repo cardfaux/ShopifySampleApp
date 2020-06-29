2019caa490cd7752d83a2580c5b705c722a0f301
cardfaux <cardfaux@tutanota.com>
2020-06-29 12:54:59 -0500

    added nodemon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 
@@ -8,8 +7,11 @@ const index = () => {
       <Layout>
         <EmptyState
           heading="Manage your inventory transfers"
-          action={{content: 'Select Products'}}
-          secondaryAction={{content: 'Learn more', url: 'https://help.shopify.com'}}
+          action={{ content: 'Select Products' }}
+          secondaryAction={{
+            content: 'Learn more',
+            url: 'https://help.shopify.com'
+          }}
           image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
         >
           <p>Select Products</p>
