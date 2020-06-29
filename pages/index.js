48962136a5df977df6fc218b6b7469b33be575cd
cardfaux <cardfaux@tutanota.com>
2020-06-29 11:42:57 -0500

    added koa and server file

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable @shopify/jsx-no-hardcoded-content */
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <p>Sample App Using React and Next.JS</p>
+    </div>
+  );
+};
+
+export default index;
