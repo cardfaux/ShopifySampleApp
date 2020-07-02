5c7b545a47ddb187469ccf092fd36fa6f0961921
cardfaux <cardfaux@tutanota.com>
2020-07-01 19:08:20 -0500

    added the bes sellers gui and css and jquery for the love of God

--- server.js
+++ server.js
@@ -1,54 +1,108 @@
 require('isomorphic-fetch');
+const dotenv = require('dotenv');
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
 const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const dotenv = require('dotenv');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
+const koaBody = require('koa-body');
 
 dotenv.config();
 const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 
-const PORT = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
-app.prepare().then(() => {
-  const server = new Koa();
-  server.use(session({ sameSite: 'none', secure: true }, server));
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+const server = new Koa();
+const router = new KoaRouter();
 
-  server.use(
-    createShopifyAuth({
-      apiKey: SHOPIFY_API_KEY,
-      secret: SHOPIFY_API_SECRET_KEY,
-      scopes: [
-        'read_products',
-        'write_products',
-        'read_script_tags',
-        'write_script_tags'
-      ],
-      afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
-        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
-        ctx.redirect('/');
-      }
-    })
-  );
+let products = [];
 
-  server.use(graphQLProxy({ version: ApiVersion.April20 }));
-  server.use(verifyRequest());
-  server.use(async (ctx) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  });
-
-  server.listen(PORT, () => {
-    console.log('\x1b[36m%s\x1b[0m', `App API Is Running At http://localhost:${PORT}`);
-  });
+router.get('/api/products', async (ctx) => {
+  try {
+    ctx.body = {
+      status: 'success',
+      data: products
+    };
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+router.post('/api/products', koaBody(), async (ctx) => {
+  try {
+    const body = ctx.request.body;
+    products.push(body);
+    ctx.body = 'Item Added';
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete('/api/products', koaBody(), async (ctx) => {
+  try {
+    products = [];
+    ctx.body = 'All items deleted!';
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Router Middleware
+server.use(router.allowedMethods());
+server.use(router.routes());
+
+app
+  .prepare()
+  .then(() => {
+    server.use(session(server));
+    // server.use(
+    //   session({ sameSite: 'none', secure: true }, (server.proxy = true))
+    // );
+    server.keys = [SHOPIFY_API_SECRET_KEY];
+
+    server.use(
+      createShopifyAuth({
+        apiKey: SHOPIFY_API_KEY,
+        secret: SHOPIFY_API_SECRET_KEY,
+        scopes: [
+          'read_products',
+          'write_products',
+          'read_script_tags',
+          'write_script_tags'
+        ],
+        afterAuth(ctx) {
+          const { shop, accessToken } = ctx.session;
+          ctx.cookies.set('shopOrigin', shop, {
+            httpOnly: false,
+            secure: true,
+            sameSite: 'none'
+          });
+          ctx.redirect('/');
+        }
+      })
+    );
+
+    server.use(graphQLProxy({ version: ApiVersion.October19 }));
+    server.use(verifyRequest());
+
+    server.use(async (ctx) => {
+      await handle(ctx.req, ctx.res);
+      ctx.respond = false;
+      ctx.res.statusCode = 200;
+    });
+
+    server.listen(port, () => {
+      console.log(
+        '\x1b[36m%s\x1b[0m',
+        `App API Is Running At http://localhost:${port}`
+      );
+    });
+  })
+  .catch((error) => console.log(error));
